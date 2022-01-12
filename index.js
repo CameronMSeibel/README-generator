0a9550296bdd505d2d0d53ea360c9ddccfe1d0e7
cmseibel <cam.m.seib@gmail.com>
2022-01-12 12:38:54 -0800

    Write general inquirer flow.

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const md = require("./generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data){
+    fs.writeFile(fileName, data);
+}
 
 // TODO: Create a function to initialize app
 function init(){
-
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            const markdown = md.generateMarkdown(answers);
+            writeToFile("README.md", markdown);
+        })
 }
 
 // Function call to initialize app
