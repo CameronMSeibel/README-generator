32c366820139fdf36883d4a0b5ddab82c0356688
cmseibel <cam.m.seib@gmail.com>
2022-01-12 15:02:51 -0800

    Write basic inquirer questions.

--- index.js
+++ index.js
@@ -9,19 +9,47 @@ const questions = [
         message: "What is your project called?"
     },
     {
-
+        name: "description",
+        message: "Please provide a brief description of your project."
+    },
+    {
+        name: "installation",
+        message: "Please provide installation instructions for your project."
+    },
+    {
+        name: "usage",
+        message: "Please provide usage information for your project."
+    },
+    {
+        name: "contribution",
+        message: "Please provide contribution guidelines."
+    },
+    {
+        name: "tests",
+        message: "Please provide test instructions for your project."
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please select a license for your project.",
+        choices: [
+            "MIT",
+            "GPL",
+            "Apache",
+            "N/A"
+        ]
     }
 ];
 
 function writeToFile(fileName, data){
-    fs.writeFile(fileName, data);
+    fs.writeFile(fileName, data, (error) => { error ? console.log(err) : console.log(`Wrote to README.md`)});
 }
 
 function init(){
     inquirer
         .prompt(questions)
         .then((answers) => {
-            const markdown = md.generateMarkdown(answers);
+            const markdown = md(answers);
             writeToFile("README.md", markdown);
         })
 }
